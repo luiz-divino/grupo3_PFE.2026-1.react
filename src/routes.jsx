c98e6079471accce371776317b52c036f59d9213
luiz-divino <nandodivinodev@gmail.com>
2026-05-29 21:59:38 -0300

    criando e configurando arquivo de rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Nav } from "./components/Nav/Nav.jsx";
 import { Home } from "./pages/Home/Home";
 import { QuemSomos } from "./pages/QuemSomos/QuemSomos";
 import { Blog } from "./pages/Blog/Blog";
@@ -6,15 +7,16 @@ import { Contato } from "./pages/Contato/Contato";
 import { Cadastro } from "./pages/Cadastro/Cadastro";
 
 export const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quem-somos" element={<QuemSomos />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contato" element={<Contato />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Nav />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/quem-somos" element={<QuemSomos />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/contato" element={<Contato />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
