dbb6212c7a24852108b75f50fa72b5d3064964a9
luiz-divino <nandodivinodev@gmail.com>
2026-05-31 17:20:53 -0300

    invocando o componente HERO e adicionando o conteudo referente ao mesmo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,34 @@
+import { Hero } from "../../components/Hero/Hero";
+import "./home.css";
+
 export const Home = () => {
-  return (
-    <>
-      <h1>PAGINA HOME</h1>
-    </>
-  );
+    return (
+        <main>
+            <Hero>
+                <div className="hero-text-container">
+                    <h1>
+                        ASSOCIAÇÃO DE{" "}
+                        <span className="highlight">CONSELHEIROS DO BRASIL</span>
+                    </h1>
+                    <div className="hero-text-description">
+                        <p>
+                            Aliamos experiência, credibilidade e conhecimento,
+                            para orientar e ajudar a construir as melhores
+                            práticas de governança corporativa e otimização de
+                            processos.
+                        </p>
+                    </div>
+                    <div className="hero-buttons">
+                        <a href="./views/cadastro.html" className="btn-primary">
+                            Quero me associar &rarr;
+                        </a>
+                        <a href="./views/quemsomos.html" className="btn-secondary">
+                            Conhecer a ACBrasil
+                        </a>
+                    </div>
+                </div>
+            </Hero>
+            <h1>PAGINA HOME</h1>
+        </main>
+    );
 };
