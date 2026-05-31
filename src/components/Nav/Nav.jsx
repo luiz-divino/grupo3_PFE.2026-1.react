bc722920b1ea7b644303055250f55d5654b45a46
luiz-divino <nandodivinodev@gmail.com>
2026-05-31 17:19:20 -0300

    melhorando organização do codigo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo-acb.png";
+import "./nav.css";
 
 export const Nav = () => {
     return (
@@ -6,38 +8,48 @@ export const Nav = () => {
             <header className="top-header">
                 <nav className="nav-container">
                     <div className="logo">
-                        <img src="#" alt="Logo da ACB" />
+                        <img src={logo} alt="Logo da ACB" />
+                    </div>
+                    {/*espaço para adicionar button menu hamburguer para telas mobile */}
+                    <button
+                        class="menu-toggle"
+                        type="button"
+                        aria-label="Abrir menu"
+                        aria-expanded="false"
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+
+                    {/* menu de navegação */}
+                    <ul className="menu">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/quem-somos">Quem Somos</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog">Blog</Link>
+                        </li>
+                        <li>
+                            <Link to="/membros">Membros</Link>
+                        </li>
+                        <li>
+                            <Link to="/contato">Contato</Link>
+                        </li>
+                    </ul>
+
+                    <div className="actions">
+                        <span className="btn-login">
+                            <Link to="/login">Entrar</Link>
+                        </span>
+                        <span className="btn-register">
+                            <Link to="/cadastro">Associe-se</Link>
+                        </span>
                     </div>
                 </nav>
-                {/*espaço para adicionar button menu hamburguer para telas mobile */}
-
-                {/* menu de navegação */}
-                <ul className="menu">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/quem-somos">Quem Somos</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog">Blog</Link>
-                    </li>
-                    <li>
-                        <Link to="/membros">Membros</Link>
-                    </li>
-                    <li>
-                        <Link to="/contato">Contato</Link>
-                    </li>
-                </ul>
-
-                <div className="actions">
-                    <button className="btn-login">
-                        <Link to="/login">Entrar</Link>
-                    </button>
-                    <button className="btn-register">
-                        <Link to="/cadastro">Associe-se</Link>
-                    </button>
-                </div>
             </header>
         </>
     );
