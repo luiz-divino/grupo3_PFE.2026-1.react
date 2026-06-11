6846d8cb7fcb157edc4311618a67f2a3129908ec
luiz-divino <nandodivinodev@gmail.com>
2026-06-11 19:01:17 -0300

    adicionar URLs das APIs financeiras

--- src/infrastructure/api/client.js
+++ src/infrastructure/api/client.js
@@ -1,1 +1,6 @@
 export const API_BASE_URL = "https://acbrasil.org.br/cms/wp-json";
+export const CDI = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.4391/dados/ultimos/1?formato=json";
+export const IPCA = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.13522/dados/ultimos/1?formato=json";
+export const SELIC = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados/ultimos/1?formato=json";
+export const USDBRL = "https://economia.awesomeapi.com.br/last/USD-BRL";
+export const POUPANCA = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados/ultimos/1?formato=json";
