ae0361844c2c300a593f1fc7358dfd3bb39bdc6a
luiz-divino <nandodivinodev@gmail.com>
2026-06-12 01:03:06 -0300

    feat(constants): add formSubmit constants

---
+++ src/constants/formSubmit.js
@@ -0,0 +1,10 @@
+export const FORM_SUBMIT_ACTION =
+    "https://formsubmit.co/devwebtms@gmail.com";
+
+export const FORM_SUBMIT_AJAX =
+    "https://formsubmit.co/ajax/devwebtms@gmail.com";
+
+export const FORM_SUBMIT_PROPS = {
+    action: FORM_SUBMIT_ACTION,
+    method: "POST",
+};
